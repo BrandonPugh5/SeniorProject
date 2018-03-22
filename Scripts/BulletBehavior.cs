1c0ca918a7761179e4719aa855a2d3c5d17197af
steinmetc8 <32173978+steinmetc8@users.noreply.github.com>
2018-03-21 23:14:38 -0400

    Organized

---
+++ Scripts/BulletBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+    float lifetime = 1.0f;
+    
+    void OnCollisionEnter2D(Collision2D objectHit)
+    {
+        string hitTag = objectHit.gameObject.tag;
+        ShrinkAndGrow shrinkAndGrow = objectHit.gameObject.GetComponent<ShrinkAndGrow>();
+        
+
+        if (hitTag == "Shootable" 
+            && GunBehavior.currentGunMode == GunBehavior.GunMode.shrink 
+            && shrinkAndGrow.canShrink)
+        {
+            objectHit.transform.localScale *= shrinkAndGrow.shrinkAmount;
+            shrinkAndGrow.currentCount--;
+            shrinkAndGrow.canGrow = true;
+            if (shrinkAndGrow.currentCount <= -1*shrinkAndGrow.maxCount)
+            {
+                shrinkAndGrow.canShrink = false;
+            }
+        }
+        else if (hitTag == "Shootable" 
+            && GunBehavior.currentGunMode == GunBehavior.GunMode.grow
+            && shrinkAndGrow.canGrow)
+        {
+            objectHit.transform.localScale *= shrinkAndGrow.growAmount;
+            shrinkAndGrow.currentCount++;
+            shrinkAndGrow.canShrink = true;
+            if(shrinkAndGrow.currentCount >= shrinkAndGrow.maxCount)
+            {
+                shrinkAndGrow.canGrow = false;
+            }
+        }
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        lifetime -= 0.7f * Time.deltaTime;
+        if (lifetime <= 0.0)
+            Destroy(gameObject);
+
+    }
+}
---
+++ Scripts/GunBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class GunBehavior : MonoBehaviour
+{
+
+    public GameObject bulletModel;
+    public GameObject gun;
+    public float speed = 5.0f;
+
+    public static GunMode currentGunMode = GunMode.shrink;
+    public enum GunMode
+    {
+        shrink,
+        grow
+    };
+
+    void Update()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector3 direction = target - gun.transform.position;
+        direction.Normalize();
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(bulletModel, gun.transform.position, rotation);
+            currentGunMode = GunMode.shrink;
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            GameObject projectile = Instantiate(bulletModel, gun.transform.position, rotation);
+            currentGunMode = GunMode.grow;
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
