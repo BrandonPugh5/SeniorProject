c998d09c188420eba908fb6e01998e7fabb65dc6
steinmetc8 <32173978+steinmetc8@users.noreply.github.com>
2018-03-31 21:15:33 -0400

    Changed how gun mode is changed
    Right-clicking or pressing 1 or 2 on keyboard or numpad changes gun mode.

--- Scripts/GunBehavior.cs
+++ Scripts/GunBehavior.cs
@@ -1,42 +1,84 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class GunBehavior : MonoBehaviour
 {
 
-    public GameObject bulletModel;
-    public GameObject gun;
-    public float speed = 5.0f;
+	public GameObject bulletModel;
+	private GameObject rightArm;
+	private GameObject dialoguePanel;
+	public float speed = 5.0f;
 
-    public static GunMode currentGunMode = GunMode.shrink;
-    public enum GunMode
-    {
-        shrink,
-        grow
-    };
+    private List<GunMode> gunModeList;
+	public bool isHeldByPlayer;
 
-    void Update()
-    {
-        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        Vector3 direction = target - gun.transform.position;
-        direction.Normalize();
-        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+	public static GunMode currentGunMode = GunMode.SHRINK;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject projectile = Instantiate(bulletModel, gun.transform.position, rotation);
-            currentGunMode = GunMode.shrink;
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-        }
-
-        if(Input.GetMouseButtonDown(1))
-        {
-            GameObject projectile = Instantiate(bulletModel, gun.transform.position, rotation);
-            currentGunMode = GunMode.grow;
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-        }
+	public enum GunMode
+	{
+		SHRINK = 1,
+		GROW
 
     }
 
+	;
+
+	private void Start ()
+	{
+		rightArm = GameObject.Find ("RightArm");
+		dialoguePanel = GameObject.Find ("Canvas").transform.Find ("DialoguePanel").gameObject;
+        gunModeList = new List<GunMode>();
+        gunModeList.AddRange((GunMode[])Enum.GetValues(typeof(GunMode)));
+    }
+
+    void Update ()
+	{
+        CheckInput();
+	}
+    
+    void CheckInput()
+    {
+        if (GunInteract.isHeldByPlayer
+            && dialoguePanel != null
+            && !dialoguePanel.activeInHierarchy)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+                Vector3 direction = target - transform.position;
+                direction.Normalize();
+                Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+
+                GameObject projectile = Instantiate(bulletModel, transform.position, rotation);
+                projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if ((int)currentGunMode == gunModeList.Count)
+                {
+                    currentGunMode = gunModeList[0];
+                }
+                else
+                {
+                    currentGunMode = gunModeList[(int)currentGunMode];
+                }
+            }
+
+            foreach (GunMode gunMode in gunModeList)
+            {
+                string gunModeString = ((int)gunMode).ToString();
+                
+                if (Input.GetKeyDown(gunModeString) 
+                    || Input.GetKeyDown("[" + gunModeString + "]"))
+                {
+                    currentGunMode = gunMode;
+                }
+            }
+
+        }
+    }
 
 }
 
