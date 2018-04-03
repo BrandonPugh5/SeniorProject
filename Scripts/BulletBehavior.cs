e6751ed62a924734e55cb2edc8005cb0f728293b
steinmetc8 <32173978+steinmetc8@users.noreply.github.com>
2018-04-03 01:39:00 -0400

    Updated scripts

--- Scripts/BulletBehavior.cs
+++ Scripts/BulletBehavior.cs
@@ -13,61 +13,101 @@ public class BulletBehavior : MonoBehaviour {
         string hitTag = objectHit.gameObject.tag;
         ShrinkAndGrow shrinkAndGrow = objectHit.gameObject.GetComponent<ShrinkAndGrow>();
     
-
-        if (hitTag == "Shootable" 
-            && GunBehavior.currentGunMode == GunBehavior.GunMode.shrink 
-            && !shrinkAndGrow.canOnlyGrow
-            && shrinkAndGrow.maxShrinkHits > 0)
+        if(hitTag == "Shootable")
         {
-            objectHit.transform.localScale *= shrinkAndGrow.shrinkScale;
-            shrinkAndGrow.maxGrowHits++;
-            shrinkAndGrow.maxShrinkHits--;
-            shrinkAndGrow.canGrow = true;
+            bool canShrink = GunBehavior.currentGunMode == GunBehavior.GunMode.SHRINK
+                && !shrinkAndGrow.canOnlyGrow
+                && shrinkAndGrow.maxShrinkHits > 0;
 
-            if (shrinkAndGrow.maxShrinkHits <= 0)
-            {
-                shrinkAndGrow.canShrink = false;
-            }
-        }
-        else if (hitTag == "Shootable" 
-            && GunBehavior.currentGunMode == GunBehavior.GunMode.grow
-            && !shrinkAndGrow.canOnlyShrink
-            && shrinkAndGrow.maxGrowHits > 0)
-        {
-            objectHit.transform.localScale *= shrinkAndGrow.growScale;
-            shrinkAndGrow.maxGrowHits--;
-            shrinkAndGrow.maxShrinkHits++;
-            shrinkAndGrow.canShrink = true;
+            bool canGrow = GunBehavior.currentGunMode == GunBehavior.GunMode.GROW
+                 && !shrinkAndGrow.canOnlyShrink
+                 && shrinkAndGrow.maxGrowHits > 0;
 
-            if(shrinkAndGrow.maxGrowHits <= 0)
+            Transform parentTransform = objectHit.transform.parent;
+
+            if (canShrink)
             {
-                shrinkAndGrow.canGrow = false;
+                Shrink(objectHit.transform, shrinkAndGrow.direction, shrinkAndGrow.shrinkScale);
+                shrinkAndGrow.maxGrowHits++;
+                shrinkAndGrow.maxShrinkHits--;                
             }
 
-            string name = objectHit.gameObject.name;
-
-            if (name == "S Butcher 2")
+            else if (canGrow)
             {
-                GameObject.FindWithTag("Chicken").GetComponent<Animator>().SetBool("roll", true);
-                banim = objectHit.gameObject.transform.Find("S Butcher Rig").GetComponent<Animator>();
-                banim.SetBool("attack", true);
+                Grow(objectHit.transform, shrinkAndGrow.direction, shrinkAndGrow.growScale);
+                shrinkAndGrow.maxGrowHits--;
+                shrinkAndGrow.maxShrinkHits++;
+                
 
-                GameObject.FindWithTag("Egg").GetComponent<Animator>().SetBool("lay", true);
-                Destroy(objectHit.gameObject, 6f);
-                Destroy(GameObject.FindWithTag("Egg").GetComponent<Animator>(), 2f);
+                string name = objectHit.gameObject.name;
+
+                if (name == "S Butcher 2")
+                {
+                    GameObject.FindWithTag("Chicken").GetComponent<Animator>().SetBool("roll", true);
+                    banim = objectHit.gameObject.transform.Find("S Butcher Rig").GetComponent<Animator>();
+                    banim.SetBool("attack", true);
+
+                    GameObject.FindWithTag("Egg").GetComponent<Animator>().SetBool("lay", true);
+                    Destroy(objectHit.gameObject, 6f);
+                    Destroy(GameObject.FindWithTag("Egg").GetComponent<Animator>(), 2f);
+                }
+
+                if (name == "pan")
+                {
+                    objectHit.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+
+                }
+
             }
-
-            if (name == "pan")
-            {
-                objectHit.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-
-            }
-
         }
 
         Destroy(gameObject);
     }
 	
+    void Shrink(Transform objectToShrink, ShrinkAndGrow.ScaleDirection direction, float shrinkScale)
+    {
+        Transform parentTransform = objectToShrink.parent;
+
+        if (direction == ShrinkAndGrow.ScaleDirection.LEFT
+            || direction == ShrinkAndGrow.ScaleDirection.RIGHT)
+        {
+            Vector2 scale = new Vector2(parentTransform.localScale.x * shrinkScale, parentTransform.localScale.y);
+            parentTransform.localScale = scale;
+        }
+        else if (direction == ShrinkAndGrow.ScaleDirection.UPWARDS
+            || direction == ShrinkAndGrow.ScaleDirection.DOWNWARDS)
+        {
+            Vector2 scale = new Vector2(parentTransform.localScale.x, parentTransform.localScale.y * shrinkScale);
+            parentTransform.localScale = scale;
+        }
+        else
+        {
+            objectToShrink.localScale *= shrinkScale;
+        }
+    }
+
+    void Grow(Transform objectToShrink, ShrinkAndGrow.ScaleDirection direction, float growScale)
+    {
+        Transform parentTransform = objectToShrink.parent;
+
+        if (direction == ShrinkAndGrow.ScaleDirection.LEFT
+            || direction == ShrinkAndGrow.ScaleDirection.RIGHT)
+        {
+            Vector2 scale = new Vector2(parentTransform.localScale.x * growScale, parentTransform.localScale.y);
+            parentTransform.localScale = scale;
+        }
+        else if (direction == ShrinkAndGrow.ScaleDirection.UPWARDS
+            || direction == ShrinkAndGrow.ScaleDirection.DOWNWARDS)
+        {
+            Vector2 scale = new Vector2(parentTransform.localScale.x, parentTransform.localScale.y * growScale);
+            parentTransform.localScale = scale;
+        }
+        else
+        {
+            objectToShrink.localScale *= growScale;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 
--- Scripts/ShrinkAndGrow.cs
+++ Scripts/ShrinkAndGrow.cs
@@ -4,12 +4,45 @@ using UnityEngine;
 
 public class ShrinkAndGrow : MonoBehaviour {
 
-    [HideInInspector]
-    public bool canGrow, canShrink;
-
+    
     public bool canOnlyGrow, canOnlyShrink;
     public int maxGrowHits, maxShrinkHits;
     public float growScale, shrinkScale;
-    
+    public ScaleDirection direction;
 
+    [HideInInspector]
+    public enum ScaleDirection
+    {
+        ALL_DIRECTIONS,
+        UPWARDS,
+        DOWNWARDS,
+        LEFT,
+        RIGHT
+    }
+
+    private void Start()
+    {
+        Vector2 position = transform.localPosition;
+        Vector2 parentPosition = transform.parent.position;
+
+        switch (direction)
+        {
+            case ScaleDirection.UPWARDS:  //change pivot of object to be at the bottom
+                transform.localPosition = new Vector2(0, 0.5f);
+                transform.parent.position = new Vector2(parentPosition.x, parentPosition.y - .5f);
+                break;
+            case ScaleDirection.DOWNWARDS:  //change pivot of object to be at the top
+                transform.position = new Vector2(0, -0.5f);
+                transform.parent.position = new Vector2(parentPosition.x, parentPosition.y + .5f);
+                break;
+            case ScaleDirection.LEFT:  //change pivot of object to be to the right
+                transform.position = new Vector2(-0.5f, 0);
+                transform.parent.position = new Vector2(parentPosition.x + 0.5f, parentPosition.y);
+                break;
+            case ScaleDirection.RIGHT:  //change pivot of object to be to the left
+                transform.position = new Vector2(0.5f, 0);
+                transform.parent.position = new Vector2(parentPosition.x - 0.5f, parentPosition.y);
+                break;
+        }
+    }
 }
